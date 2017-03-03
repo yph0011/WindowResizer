ea01888c933906d94c688b4c87f4ce86209069ab
Kazuya.Mitsutani <Kazuya.Mitsutani@gmail.com>
2017-03-03 23:27:26 +0900

    [feat]　フォームに属さない機能をライブラリ化する

--- WindowsFormsApplication1/Controller.cs
+++ WindowsFormsApplication1/Controller.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibWindowInfo;
 
 namespace WindowsFormsApplication1
 {
@@ -19,15 +21,15 @@ namespace WindowsFormsApplication1
 
         private void ListUpWindows_Click(object sender, EventArgs e)
         {
-            foreach(System.Diagnostics.Process p in
-                System.Diagnostics.Process.GetProcesses())
+            WindowManager winman = new WindowManager();
+            List<Process> pList = winman.GetProcessesWithWIndow();
+            int i = 0;
+            foreach(Process p in pList)
             {
-                if (p.MainWindowTitle.Length != 0)
-                {
-
-                    TargetWindowList.Items.Add(p.ProcessName + ':' +p.MainWindowTitle);
-                }
+                string lblstr = p.ProcessName + " : " + p.MainWindowTitle;
+                TargetWindowList.Items.Add(lblstr);
             }
+            TargetWindowList.Refresh();
         }
 
     }
