2180ddda6fbcdcc160b7a61d9864ef2a1274fab5
Kazuya.Mitsutani <Kazuya.Mitsutani@gmail.com>
2017-03-03 23:28:02 +0900

    Library to manage window infomations

---
+++ ClassLibrary1/LibWindowInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibWindowInfo;
+
+namespace LibWindowInfo 
+{
+    public class WindowManager
+
+    {
+        public List<Process> GetProcessesWithWIndow()
+        {
+            List<Process> pList = new List<Process>(); 
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.MainWindowTitle.Length != 0) pList.Add(p);
+            }
+            return pList;
+        }
+    }
+}
